02fed44b999f207a44c26497e5794223e60458a6
clownfish7 <279505647@qq.com>
2021-07-23 10:11:33 +0800

    Initial commit

---
+++ src/main/java/com/clownfish7/disruptor/DisruptorApplication.java
@@ -0,0 +1,13 @@
+package com.clownfish7.disruptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisruptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisruptorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clownfish7/disruptor/DisruptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clownfish7.disruptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisruptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
